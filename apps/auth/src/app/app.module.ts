6e9e0e53353dd9a14cbc3bb6253f87f1320c9e00
I-yan-u <iyanuajimobi12@gmail.com>
2026-03-13 11:37:17 +0100

    Add appConfig to ConfigModule loading in AppModule

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { serviceConfig, dbConfig } from '@medicpadi-backend/config';
+import { serviceConfig, dbConfig, appConfig } from '@medicpadi-backend/config';
 import { Auth } from './auth/entities/auth.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
@@ -12,17 +12,18 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `medicpadi-backend/apps/auth/.env.${process.env.NODE_ENV || 'development'}`,
-      load: [serviceConfig, dbConfig],
+      load: [serviceConfig, dbConfig, appConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
-        console.log(
-          'Service configuration:',
-          configService.get('serviceConfig'),
-        );
-        console.log('Database configuration:', configService.get('dbConfig'));
+        // console.log(
+        //   'Service configuration:',
+        //   configService.get('serviceConfig'),
+        // );
+        // console.log('Database configuration:', configService.get('dbConfig'));
+        console.log('All congigs', configService);
         return {
           type: 'postgres',
           entities: [Auth],
